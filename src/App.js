3e19699ae3f8398e432a62a03b571ecc137d5131
minhtam <minhtamnhgp03@gmail.com>
2024-10-27 13:46:49 +0700

    grid system

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import CreatorUI from "./components/CreatorUI";
-import "bootstrap/dist/css/bootstrap.min.css";
-import CreatorUI2 from "./components/CreatorUI2";
-import ReadingTaskUI from "./components/ReadingTaskUI";
-import ReadingTask from "./components/ReadingTask";
-import ReadingTaskUI2 from "./components/ReadingTaskUI2";
-import ReadingTaskUI3 from "./components/ReadingTaskUI3";
-import CreatorUI1 from "./components/CreatorUI1";
+import { Box } from "@mui/material";
+import React from "react";
 import Column1 from "./components/Column1";
 import Column2 from "./components/Column2";
 import Column3 from "./components/Column3";
 import Column4 from "./components/Column4";
 import Column5 from "./components/Column5";
 import Column6 from "./components/Column6";
+import Column7 from "./components/Column7";
 
 function App() {
   return (
-    <>
-      <Column1 />
-      <Column2 />
-      <Column3 />
-      <Column4 />
-      <Column5 />
-      <Column6 />
-      <CreatorUI />
-      <CreatorUI1 />
-      <CreatorUI2 />
-      <ReadingTaskUI />
-      <ReadingTask />
-      <ReadingTaskUI2 />
-      <ReadingTaskUI3 />
-    </>
+    <Box
+      display="flex" // Arrange items in a horizontal row
+      overflow="auto" // Enable horizontal scrolling
+      sx={{ whiteSpace: "nowrap" }} // Prevent wrapping to a new row
+    >
+      <Box sx={{ minWidth: 360, padding: 2 }}>
+        <Column1 />
+      </Box>
+      <Box sx={{ minWidth: 360, padding: 2 }}>
+        <Column2 />
+      </Box>
+      <Box sx={{ minWidth: 360, padding: 2 }}>
+        <Column3 />
+      </Box>
+      <Box sx={{ minWidth: 360, padding: 2 }}>
+        <Column4 />
+      </Box>
+      <Box sx={{ minWidth: 360, padding: 2 }}>
+        <Column5 />
+      </Box>
+      <Box sx={{ minWidth: 360, padding: 2 }}>
+        <Column6 />
+      </Box>
+      <Box sx={{ minWidth: 360, padding: 2 }}>
+        <Column7 />
+      </Box>
+    </Box>
   );
 }
 
